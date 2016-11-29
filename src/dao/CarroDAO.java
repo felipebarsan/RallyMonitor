bb003c391475785465e11f630b3f43e24d56bad9
Felipe <Felipe@Felipe-PC>
2016-11-28 22:20:34 -0200

    Teste de envio.

---
+++ src/dao/CarroDAO.java
@@ -0,0 +1,40 @@
+package dao;
+
+public class CarroDAO {
+	
+/*
+ * 
+ * 
+ *  select que retorna lista de carros	
+	SELECT `carros`.`idcarro`,
+    `carros`.`carromarca`,
+    `carros`.`carronumero`
+FROM `rally`.`carros`;
+
+ insert
+ * INSERT INTO `rally`.`carros`
+(`idcarro`,
+`carromarca`,
+`carronumero`)
+VALUES
+(<{idcarro: }>,
+<{carromarca: }>,
+<{carronumero: }>);
+
+update
+UPDATE `rally`.`carros`
+SET
+`idcarro` = <{idcarro: }>,
+`carromarca` = <{carromarca: }>,
+`carronumero` = <{carronumero: }>
+WHERE `idcarro` = <{expr}>;
+ 
+ deletar
+ 
+ DELETE FROM `rally`.`carros`
+WHERE <{where_expression}>;
+
+
+ */
+	
+}
